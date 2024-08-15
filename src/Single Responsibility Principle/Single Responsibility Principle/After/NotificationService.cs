76942fd92f555fedd6cca8249f74db5ab5fc52cd
Zahra Abbasifard <z.abbasifard@hotmail.com>
2024-08-15 22:29:28 +0330

    implement single responsibility Principle

---
+++ src/Single Responsibility Principle/Single Responsibility Principle/After/NotificationService.cs
@@ -0,0 +1,49 @@
+﻿using Single_Responsibility_Principle.After;
+using System.Net.Mail;
+
+namespace Single_Responsibility_Principle.After
+{
+    public interface INotificatoin
+    {
+        void Send(string message, string receiveAddress);
+    }
+    public class NotificationEmailService : INotificatoin
+    {
+        public void Send(string message, string receiveAddress)
+        {
+            HttpClient client = new HttpClient();
+            client.Send(new HttpRequestMessage { });
+        }
+    }
+
+    public class NotificationSmsService : INotificatoin
+    {
+        public void Send(string message, string receiveAddress)
+        {
+            SmtpClient smtpClient = new SmtpClient()
+            {
+                Host = ""
+            };
+            smtpClient.Send(new MailMessage(message, receiveAddress));
+        }
+    }
+
+}
+
+public class OrderProcessor
+{
+    private readonly INotificatoin _smsService;
+    private readonly INotificatoin _emailService;
+    public OrderProcessor()
+    {
+        _smsService = new NotificationSmsService();
+        _emailService = new NotificationEmailService();
+    }
+    public void ProccesOrder(int quantity, string item, string phoneNumber, string emailAddress)
+    {
+        //implement business logic 
+
+        _smsService.Send($"your order for {quantity} {item} has been processed", phoneNumber);
+        _emailService.Send($"your order for {quantity} {item} has been processed", emailAddress);
+    }
+}
---
+++ src/Single Responsibility Principle/Single Responsibility Principle/Before/NotificationService.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Mail;
+
+namespace Single_Responsibility_Principle.Before
+{
+    //befor using Solid Principle
+    public class NotificationService
+    {
+        public void SendEmailNotification(string message, string emailAddress)
+        {
+            SmtpClient smtpClient = new SmtpClient()
+            {
+                Host = ""
+            };
+            smtpClient.Send(new MailMessage(message, emailAddress));
+
+        }
+
+        public void SendSmsNotification(string message, string phoneNumber)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.Send(new HttpRequestMessage { });
+        }
+
+    }
+
+    public class OrderProcessor
+    {
+        private readonly NotificationService _notificationService;
+
+        public OrderProcessor() => _notificationService = new NotificationService();
+
+        public void ProcessOrder(int quantity, string item, string phoneNumber, string emailAddress)
+        {
+            //implement business logic 
+
+            _notificationService.SendSmsNotification($"your order for {quantity} {item} has been processed", phoneNumber);
+            _notificationService.SendEmailNotification($"your order for {quantity} {item} has been processed", emailAddress);
+        }
+
+    }
+}
+
